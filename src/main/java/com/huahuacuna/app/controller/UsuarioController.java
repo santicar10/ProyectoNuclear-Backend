cf00fb9428cebf8d761fd4465a3c2b10c6982570
Jan Carlos Guevara Perdomo <jguevara8076@cue.edu.co>
2025-10-23 11:32:30 -0500

    test commit for cors

--- src/main/java/com/huahuacuna/app/controller/UsuarioController.java
+++ src/main/java/com/huahuacuna/app/controller/UsuarioController.java
@@ -30,7 +30,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/api/usuarios")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000\", allowCredentials = \"true")
 public class UsuarioController {
 
     /**
